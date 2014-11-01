1fbdd6f09530a3738e21b2041c9da32a6854b202
mike <mcwebmike@gmail.com>
2014-11-01 08:03:57 -0600

    crea la vista (api) de camiones

--- drove/api/views.py
+++ drove/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from transporte.models import Camion
+from api.serializers import CamionSerializer
+
+
+class CamionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint que permite ver y editar camiones.
+    """
+    queryset = Camion.objects.all()
+    serializer_class = CamionSerializer
