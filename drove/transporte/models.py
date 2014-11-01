4c4e753198b8c3a5d6451deaa36b8ab861f4efb9
mike <mcwebmike@gmail.com>
2014-11-01 03:35:50 -0600

    Crea modelos iniciales

--- drove/transporte/models.py
+++ drove/transporte/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Ruta(models.Model):
+    nombre_ruta = models.CharField(max_length=25)
+    num_camiones = models.IntegerField(blank=True, null=True)
+    num_pasajeros = models.IntegerField(blank=True, null=True)
+    promedio_personas = models.FloatField(blank=True, null=True)
+    promedio_personas_camion = models.FloatField(blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.nombre_ruta
+    
+    
+class Parada(models.Model):
+    nombre_parada = models.CharField(max_length=25)
+    longitud = models.CharField(max_length=140)
+    latitud = models.CharField(max_length=140)
+    ruta = models.ManyToManyField('Ruta')
+    
+    def __unicode__(self):
+        return self.nombre_parada
+    
+
+class Camion(models.Model):
+    identificador = models.CharField(max_length=32)
+    velocidad = models.IntegerField(null=True)
+    num_pasajeros = models.IntegerField(null=True)
+    promedio_tiempo = models.FloatField(null=True)
+    promedio_pasajeros = models.FLoatField(null=True)
+    ruta = models.ForeignKey('Ruta')
+    
+    def __unicode__(self):
+        return self.identificador
+    
+    
+
+
